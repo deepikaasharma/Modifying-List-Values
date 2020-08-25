37d4747b3a15c019cf6eaab5afea46b0e2693b1f
Deepika Sharma <deepikaasharma@outlook.com>
2020-08-25 09:41:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+list_of_birds = ['peacock', 'rooster', 'bluejay', 'cardinal']
+
+# modify the first element
+list_of_birds[0] = 'sparrow'
+
+print(f'the first bird in the list is a {list_of_birds[0]}')
+print(f'the second bird in the list is a {list_of_birds[1]}')
+print(f'the third bird in the list is a {list_of_birds[2]}')
+print(f'the fourth bird in the list is a {list_of_birds[3]}')
